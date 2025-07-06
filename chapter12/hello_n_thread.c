b23c28341b06b9ea5372e01bc76d4ffc618e2f0c
Robsss <Robsss.gmail.com>
2025-07-06 22:03:28 +0800

    update the code implementation with heap rather than stack for the unkonwn size of array for tid

--- chapter12/hello_n_thread.c
+++ chapter12/hello_n_thread.c
@@ -5,6 +5,7 @@ void *thread(void *vargp); /* Thread routine prototype */
 
 int main(int argc, char **argv)
 {
+    pthread_t *tid_arr;
    
     /* Check input argument */
     if (argc != 2)
@@ -18,7 +19,10 @@ int main(int argc, char **argv)
         printf("Please provide a positive integer for the number of threads.\n");
         exit(0);
     }
-    pthread_t tid_arr[n]; /* Array to hold thread IDs */
+    // allocate the unknow size array before execution on heap
+    tid_arr = Malloc(n * sizeof(pthread_t));
+    
+    //pthread_t tid_arr[n]; /* Array to hold thread IDs */
     /* Create thread and wait for it to finish */
     for(int i = 0; i < n ; i++) {
         Pthread_create(&tid_arr[i], NULL, thread, NULL);
